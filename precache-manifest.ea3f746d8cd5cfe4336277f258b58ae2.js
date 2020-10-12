5109bf41946557513c4596e76ffe3f864f83f313
Tafhim Hasan <66793356+tafhimhasan@users.noreply.github.com>
2020-10-12 17:55:49 +0600

    Updates

---
+++ precache-manifest.ea3f746d8cd5cfe4336277f258b58ae2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a99c9c8fb5a8df6160b8f1985509b47b",
+    "url": "/Simple-Chat-Room/index.html"
+  },
+  {
+    "revision": "b1092fd86cc13e0be6c2",
+    "url": "/Simple-Chat-Room/static/css/main.4d384d61.chunk.css"
+  },
+  {
+    "revision": "b658c930456ab7335830",
+    "url": "/Simple-Chat-Room/static/js/2.7aa0078b.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/Simple-Chat-Room/static/js/2.7aa0078b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1092fd86cc13e0be6c2",
+    "url": "/Simple-Chat-Room/static/js/main.dda965db.chunk.js"
+  },
+  {
+    "revision": "a62a4e7780fa4746f3bb",
+    "url": "/Simple-Chat-Room/static/js/runtime-main.6502a386.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Simple-Chat-Room/static/media/logo.5d5d9eef.svg"
+  }
+]);
